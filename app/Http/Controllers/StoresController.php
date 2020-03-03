0443ef5b36df27f2f143f643f28d13f4d077d464
Alex <alex_rodriguez_apps1ma1819@cev.com>
2020-03-03 11:07:55 +0100

    locations post ended

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ApplicationsController;
 use App\User;
 use App\Application;
+use App\Store;
 use App\Usage;
 use Illuminate\Http\Request;
 
@@ -13,51 +14,69 @@ class StoresController extends Controller
 {
     public function postLocations(Request $request)
     {
-        $appsInfo = new ApplicationsController;
-        $appsFullInfo = $appsInfo->readCSVinfo($request);
-        $arrayNames = array_values(array_reverse($appsFullInfo[0]));
-        $arrayTimes = array_values(array_reverse($appsFullInfo[1]));
-        $arrayStatus = array_values(array_reverse($appsFullInfo[2]));
-        $arrayLatitude = array_values(array_reverse($appsFullInfo[3]));
-        $arrayLongitude = array_values(array_reverse($appsFullInfo[4]));
+        try {
+            $appsInfo = new ApplicationsController;
+            $appsFullInfo = $appsInfo->readCSVinfo($request);
+            $arrayNames = array_values(array_reverse($appsFullInfo[0]));
+            $arrayStatus = array_values(array_reverse($appsFullInfo[2]));
+            $arrayLatitude = array_values(array_reverse($appsFullInfo[3]));
+            $arrayLongitude = array_values(array_reverse($appsFullInfo[4]));
+            $arrayOpenLatitudes = [];
+            $arrayOpenLongitudes = [];
+            $arrayCloseLatitudes = [];
+            $arrayCloseLongitudes = [];
 
-        $arrayTimeIntervaleOfADay = [];
-        $arrayDates = [];
-        $arrayOpenLatitudes = [];
-        $arrayOpenLongitudes = [];
-        $arrayCloseLatitudes = [];
-        $arrayCloseLongitudes = [];
-        $arrayContainerOfAllInfo = [];
+            $arrayFinalNames = array_values(array_unique($arrayNames));
+            $actualAppOnList = $arrayNames[0];
 
+            for ($i = 0; $i < count($arrayFinalNames); $i++) {
+                $actualFinalApp = $arrayFinalNames[$i];
 
-        $arrayFinalNames = array_values(array_unique($arrayNames));
-        $actualAppOnList = $arrayNames[0];
-
-        for ($i = 0; $i < count($arrayFinalNames); $i++) {
-            $actualFinalApp = $arrayFinalNames[$i];
-
-            for ($j = 0; $j < count($arrayNames); $j++) {
-                if ($actualFinalApp == $actualAppOnList && $arrayStatus[$j] == "closes") {
-                    array_push($arrayCloseLatitudes, $arrayLatitude[$j]);
-                    array_push($arrayCloseLongitudes, $arrayLongitude[$j]);
+                for ($j = 0; $j < count($arrayNames); $j++) {
                     $actualAppOnList = $arrayNames[$j];
-                    var_dump($actualAppOnList);
-                } else if ($actualFinalApp == $actualAppOnList && $arrayStatus[$j] == "opens") {
-                    array_push($arrayOpenLatitudes, $arrayLatitude[$j]);
-                    array_push($arrayOpenLongitudes, $arrayLongitude[$j]);
-                    $actualAppOnList = $arrayNames[$j];
-                    var_dump($actualAppOnList);
+                    if ($actualFinalApp == $actualAppOnList && $arrayStatus[$j] == "closes") {
+                        array_push($arrayCloseLatitudes, $arrayLatitude[$j]);
+                        array_push($arrayCloseLongitudes, $arrayLongitude[$j]);
+                    } else if ($actualFinalApp == $actualAppOnList && $arrayStatus[$j] == "opens") {
+                        array_push($arrayOpenLatitudes, $arrayLatitude[$j]);
+                        array_push($arrayOpenLongitudes, $arrayLongitude[$j]);
+                        break;
+                    } else {
+                    }
+                }
+                $user = new User;
+                $userController = new UserController;
+                $user = $userController->getUserFromToken($request);
+                if (!Store::where("user_id", $user->id)->where("application_id", $i+1)) {
+                    $store = new Store;
+                    $store->user_id = $user->id;
+                    $store->application_id = $i + 1;
+                    $store->open_latitude = $arrayOpenLatitudes[$i];
+                    $store->open_longitude = $arrayOpenLongitudes[$i];
+                    $store->close_latitude = $arrayCloseLatitudes[$i];
+                    $store->close_longitude = $arrayCloseLongitudes[$i];
+
+                    $store->save();
                 } else {
-                    var_dump($actualAppOnList);
+                    Store::where("user_id", $user->id)->where("application_id", $i+1)->delete();
+                    $store = new Store;
+                    $store->user_id = $user->id;
+                    $store->application_id = $i + 1;
+                    $store->open_latitude = $arrayOpenLatitudes[$i];
+                    $store->open_longitude = $arrayOpenLongitudes[$i];
+                    $store->close_latitude = $arrayCloseLatitudes[$i];
+                    $store->close_longitude = $arrayCloseLongitudes[$i];
+
+                    $store->save();
                 }
             }
+                return response()->json([
+                    'message' => "Locations uploaded succesfully"
+                ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => "Not possible to upload locations"
+            ], 401);
         }
-
-
-
-
-        $user = new User;
-        $userController = new UserController;
-        $user = $userController->getUserFromToken($request);
     }
 }
